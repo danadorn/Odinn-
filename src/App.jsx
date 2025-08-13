e1a769bc8e23b7a3c236ed3ddecda6a3115736a2
dorndana8899@gmail.com <dorndana8899@gamil.com>
2025-08-13 11:02:36 +0700

    b

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import "./App.css";
 import { useGetProductsQuery } from "./features/product/productSlice2";
 import CardProduct from "./components/card/card-product";
 import SkeletonCardProduct from "./components/card/skeleton-card-product";
+import SEO from "./components/seo/SEOComponent";
 
 function App() {
-  const { data, isLoading } = useGetProductsQuery();
+  const { data, isLoading } = useGetProductsQuery({
+    page: 0, 
+    size: 8
+  });
   const array = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
-    <>
+    <div>
+      {/* implment SEO */}
+      <SEO
+        title="Home | ODINN"
+        description="ODINN is the most popular powerful ecommerce website which servers many Khmer products such as rural area"
+        keywords="product, category, ecommerce, khmer-product"
+        url="/"
+        image="https://opengraph.b-cdn.net/production/images/8d595c0c-58d2-444e-a8cb-fb56828d10c7.png?token=Ure-YZdf_B4w_ShGpptIcKtMwt92WbwrJuuy9d6RNGg&height=675&width=1200&expires=33291054974"
+
+      />
       <main className="max-w-screen-xl mx-auto">
         <section className="grid grid-cols-4 gap-5">
           {isLoading &&
@@ -25,7 +38,7 @@ function App() {
             ))}
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
